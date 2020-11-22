ef28bb62a729c5e00a1feca67a13d7779781ae97
Brennan Predmore <brennnann@gmail.com>
2020-11-21 19:03:35 -0600

    reformat code with prettier // remove unused comments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
-import { Icon} from '@iconify/react'
-import locationIcon from '@iconify/icons-mdi/fire-alert'
+import { Icon } from '@iconify/react';
+import locationIcon from '@iconify/icons-mdi/fire-alert';
 
 const Header = () => {
-    return (
-        <header className="header">
-            <h1><Icon icon={locationIcon} /> WildFire Tracker (Powered by NASA)<Icon icon={locationIcon} /></h1>
-            
-        </header>
-    )
-}
+  return (
+    <header className='header'>
+      <h1>
+        <Icon icon={locationIcon} /> WildFire Tracker (Powered by NASA)
+        <Icon icon={locationIcon} />
+      </h1>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
